7b358b90b712c9f9afe0f18e6b25d10b61706639
olegspace <olezhka_revin@mail.ru>
2024-10-19 02:19:23 +0700

    Fix bag with sesion collision and delete min max fields

--- Cinema/AddSessionForm.Designer.cs
+++ Cinema/AddSessionForm.Designer.cs
@@ -31,16 +31,12 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.sessionStartDateTimePicker = new System.Windows.Forms.DateTimePicker();
-            this.minPriceMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
             this.hallComboBox = new System.Windows.Forms.ComboBox();
             this.createSessionButton = new System.Windows.Forms.Button();
             this.filmComboBox = new System.Windows.Forms.ComboBox();
             this.filmBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.maxPriceMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
-            this.maxPriceLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.basePriceNUD = new System.Windows.Forms.NumericUpDown();
@@ -52,7 +48,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Location = new System.Drawing.Point(60, 16);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 15);
@@ -62,27 +58,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 38);
+            this.label2.Location = new System.Drawing.Point(60, 39);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(116, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Начало кинопоказа";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 100);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(156, 15);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Минимальная цена билета";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 171);
+            this.label5.Location = new System.Drawing.Point(60, 119);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 15);
@@ -93,36 +79,25 @@
             // 
             this.sessionStartDateTimePicker.CustomFormat = "dd/MM/yyyy HH:mm";
             this.sessionStartDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.sessionStartDateTimePicker.Location = new System.Drawing.Point(179, 35);
+            this.sessionStartDateTimePicker.Location = new System.Drawing.Point(231, 36);
             this.sessionStartDateTimePicker.Margin = new System.Windows.Forms.Padding(2);
             this.sessionStartDateTimePicker.Name = "sessionStartDateTimePicker";
-            this.sessionStartDateTimePicker.Size = new System.Drawing.Size(178, 23);
+            this.sessionStartDateTimePicker.Size = new System.Drawing.Size(208, 23);
             this.sessionStartDateTimePicker.TabIndex = 6;
-            // 
-            // minPriceMaskedTextBox
-            // 
-            this.minPriceMaskedTextBox.Enabled = false;
-            this.minPriceMaskedTextBox.Location = new System.Drawing.Point(179, 93);
-            this.minPriceMaskedTextBox.Margin = new System.Windows.Forms.Padding(2);
-            this.minPriceMaskedTextBox.Mask = "00000";
-            this.minPriceMaskedTextBox.Name = "minPriceMaskedTextBox";
-            this.minPriceMaskedTextBox.Size = new System.Drawing.Size(178, 23);
-            this.minPriceMaskedTextBox.TabIndex = 7;
-            this.minPriceMaskedTextBox.ValidatingType = typeof(int);
             // 
             // hallComboBox
             // 
             this.hallComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.hallComboBox.FormattingEnabled = true;
-            this.hallComboBox.Location = new System.Drawing.Point(179, 169);
+            this.hallComboBox.Location = new System.Drawing.Point(231, 117);
             this.hallComboBox.Margin = new System.Windows.Forms.Padding(2);
             this.hallComboBox.Name = "hallComboBox";
-            this.hallComboBox.Size = new System.Drawing.Size(178, 23);
+            this.hallComboBox.Size = new System.Drawing.Size(208, 23);
             this.hallComboBox.TabIndex = 9;
             // 
             // createSessionButton
             // 
-            this.createSessionButton.Location = new System.Drawing.Point(26, 214);
+            this.createSessionButton.Location = new System.Drawing.Point(103, 173);
             this.createSessionButton.Margin = new System.Windows.Forms.Padding(2);
             this.createSessionButton.Name = "createSessionButton";
             this.createSessionButton.Size = new System.Drawing.Size(320, 53);
@@ -135,41 +110,20 @@
             // 
             this.filmComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.filmComboBox.FormattingEnabled = true;
-            this.filmComboBox.Location = new System.Drawing.Point(179, 8);
+            this.filmComboBox.Location = new System.Drawing.Point(231, 9);
             this.filmComboBox.Margin = new System.Windows.Forms.Padding(2);
             this.filmComboBox.Name = "filmComboBox";
-            this.filmComboBox.Size = new System.Drawing.Size(178, 23);
+            this.filmComboBox.Size = new System.Drawing.Size(208, 23);
             this.filmComboBox.TabIndex = 12;
             // 
             // filmBindingSource
             // 
             this.filmBindingSource.DataSource = typeof(Cinema.Film);
             // 
-            // maxPriceMaskedTextBox
-            // 
-            this.maxPriceMaskedTextBox.Enabled = false;
-            this.maxPriceMaskedTextBox.Location = new System.Drawing.Point(179, 118);
-            this.maxPriceMaskedTextBox.Margin = new System.Windows.Forms.Padding(2);
-            this.maxPriceMaskedTextBox.Mask = "00000";
-            this.maxPriceMaskedTextBox.Name = "maxPriceMaskedTextBox";
-            this.maxPriceMaskedTextBox.Size = new System.Drawing.Size(178, 23);
-            this.maxPriceMaskedTextBox.TabIndex = 14;
-            this.maxPriceMaskedTextBox.ValidatingType = typeof(int);
-            // 
-            // maxPriceLabel
-            // 
-            this.maxPriceLabel.AutoSize = true;
-            this.maxPriceLabel.Location = new System.Drawing.Point(8, 124);
-            this.maxPriceLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.maxPriceLabel.Name = "maxPriceLabel";
-            this.maxPriceLabel.Size = new System.Drawing.Size(160, 15);
-            this.maxPriceLabel.TabIndex = 13;
-            this.maxPriceLabel.Text = "Максимальная цена билета";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 149);
+            this.label3.Location = new System.Drawing.Point(60, 97);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(162, 15);
@@ -183,10 +137,10 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Расстояние места до первого ряда",
             "Близость места к центру"});
-            this.comboBox1.Location = new System.Drawing.Point(179, 144);
+            this.comboBox1.Location = new System.Drawing.Point(231, 92);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(178, 23);
+            this.comboBox1.Size = new System.Drawing.Size(208, 23);
             this.comboBox1.TabIndex = 16;
             // 
             // basePriceNUD
@@ -196,7 +150,7 @@
             0,
             0,
             0});
-            this.basePriceNUD.Location = new System.Drawing.Point(179, 63);
+            this.basePriceNUD.Location = new System.Drawing.Point(231, 64);
             this.basePriceNUD.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -208,7 +162,7 @@
             0,
             0});
             this.basePriceNUD.Name = "basePriceNUD";
-            this.basePriceNUD.Size = new System.Drawing.Size(120, 23);
+            this.basePriceNUD.Size = new System.Drawing.Size(208, 23);
             this.basePriceNUD.TabIndex = 17;
             this.basePriceNUD.Value = new decimal(new int[] {
             100,
@@ -219,7 +173,7 @@
             // basePricelabel
             // 
             this.basePricelabel.AutoSize = true;
-            this.basePricelabel.Location = new System.Drawing.Point(8, 64);
+            this.basePricelabel.Location = new System.Drawing.Point(60, 65);
             this.basePricelabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.basePricelabel.Name = "basePricelabel";
             this.basePricelabel.Size = new System.Drawing.Size(120, 15);
@@ -230,20 +184,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(390, 278);
+            this.ClientSize = new System.Drawing.Size(518, 237);
             this.Controls.Add(this.basePricelabel);
             this.Controls.Add(this.basePriceNUD);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.maxPriceMaskedTextBox);
-            this.Controls.Add(this.maxPriceLabel);
             this.Controls.Add(this.filmComboBox);
             this.Controls.Add(this.createSessionButton);
             this.Controls.Add(this.hallComboBox);
-            this.Controls.Add(this.minPriceMaskedTextBox);
             this.Controls.Add(this.sessionStartDateTimePicker);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -261,16 +211,12 @@
 
         private Label label1;
         private Label label2;
-        private Label label4;
         private Label label5;
         private DateTimePicker sessionStartDateTimePicker;
-        private MaskedTextBox minPriceMaskedTextBox;
         private ComboBox hallComboBox;
         private Button createSessionButton;
         private ComboBox filmComboBox;
         private BindingSource filmBindingSource;
-        private MaskedTextBox maxPriceMaskedTextBox;
-        private Label maxPriceLabel;
         private Label label3;
         private ComboBox comboBox1;
         private NumericUpDown basePriceNUD;
--- Cinema/AddSessionForm.cs
+++ Cinema/AddSessionForm.cs
@@ -35,7 +35,7 @@ namespace Cinema
 
             // Retrieve entered details
             movieName = filmComboBox.Text;
-            dateAndTime = sessionStartDateTimePicker.Value;
+            dateAndTime = DateTimeWithoutMinutes(sessionStartDateTimePicker.Value);
             hallNumber = hallComboBox.Text;
 
             if (!ValidateFormData())
@@ -44,17 +44,19 @@ namespace Cinema
             }            
 
             basePrice = (int)basePriceNUD.Value;
-            //minPrice = int.Parse(minPriceMaskedTextBox.Text);
-            //minPrice = int.Parse(minPriceMaskedTextBox.Text);
 
             Film f = controller.FindFilm(movieName);
             duration = f.duration;
 
+            if (IsSessionHasTimeCollision())
+            {
+                MessageBox.Show("Данный зал на время создаваемого кинопоказа занят. Попробуйте назначить другое время или другой зал.", "Пересечение времени в зале");
+                return;
+            }
+
             PricePolicy pp = ChoosePricePolicy();
             if (f != null)
             {
-                // Add a new film session to the controller
-                // controller.AddFilmSession(f, dateAndTime, hallNumber, minPrice, maxPrice, pp);
                 controller.AddFilmSession(f, dateAndTime, hallNumber, basePrice, pp);
             }
             else 
@@ -62,8 +64,12 @@ namespace Cinema
                 MessageBox.Show("Невозможно найти фильм с навзанием " + movieName, "Ошибка данных");
             }
 
-            // Close the form
             Close();
+        }
+
+        private DateTime DateTimeWithoutMinutes(DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
         }
 
         private PricePolicy ChoosePricePolicy()
@@ -89,18 +95,6 @@ namespace Cinema
                 return false;
             }
 
-            //if (string.IsNullOrWhiteSpace(minPriceMaskedTextBox.Text))
-            //{
-            //    MessageBox.Show("Поле с минимальной ценой билета не может быть пустым");
-            //    return false;
-            //}
-
-            //if (string.IsNullOrWhiteSpace(maxPriceMaskedTextBox.Text))
-            //{
-            //    MessageBox.Show("Поле с максимальной ценой билета не может быть пустым");
-            //    return false;
-            //}
-
             if (string.IsNullOrWhiteSpace(comboBox1.Text))
             {
                 MessageBox.Show("Поле с политикой ценообразования не может быть пустым");
@@ -122,52 +116,61 @@ namespace Cinema
             try
             {
                 basePrice = (int)basePriceNUD.Value;
-                //minPrice = int.Parse(minPriceMaskedTextBox.Text);
-                //maxPrice = int.Parse(maxPriceMaskedTextBox.Text);
+
             }
             catch
             {
                 MessageBox.Show("Ошибка в формате вводимых данных");
                 return false;
             }
-          
-            //if (minPrice > maxPrice)
-            //{
-            //    MessageBox.Show("Минимальная цена должна быть меньше максимальной");
-            //    return false;
-            //}
-
-            if (IsSessionHasTimeCollision())
-            {
-                MessageBox.Show("Данный зал на время создаваемого кинопоказа занят. Попробуйте назначить другое время или другой зал.", "Пересечение времени в зале");
-                return false;
-            }
-
 
             return true;
         }
 
+        //private bool IsSessionHasTimeCollision()
+        //{
+        //    var potentialCollisions = controller.GetFilmSessions().Where(fsc => fsc.GetHall().Name == hallComboBox.Text).ToList();
+
+        //    foreach (var fsc in potentialCollisions)
+        //    {
+        //        DateTime start1 = fsc.GetSessionDate();
+        //        DateTime start2 = dateAndTime;
+
+        //        DateTime end1 = start1 + fsc.GetDuration();
+        //        DateTime end2 = start2 + duration;
+
+        //        // Проверка на пересечение
+        //        if ((start1 >= start2 && start1 < end2) || (end1 > start2 && end1 <= end2) || (start1 <= start2 && end1 >= end2))
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
         private bool IsSessionHasTimeCollision()
         {
-            var potentialCollisions = controller.GetFilmSessions().Where(fsc => fsc.GetHall().Name == hallComboBox.Text).ToList();
+            var potentialCollisions = controller.GetFilmSessions()
+                .Where(fsc => fsc.GetHall().Name == hallComboBox.Text).ToList();
 
             foreach (var fsc in potentialCollisions)
             {
-                DateTime start1 = fsc.GetSessionDate();
-                DateTime start2 = dateAndTime;
+                DateTime start1 = fsc.GetSessionDate();   // Время начала текущего кинопоказа
+                DateTime start2 = dateAndTime;            // Время начала нового кинопоказа
 
-                DateTime end1 = start1 + fsc.GetDuration();
-                DateTime end2 = start2 + duration;
+                DateTime end1 = start1 + fsc.GetDuration(); // Время окончания текущего кинопоказа
+                DateTime end2 = start2 + duration;          // Время окончания нового кинопоказа
 
-                // Проверка на пересечение
-                if ((start1 >= start2 && start1 < end2) || (end1 > start2 && end1 <= end2) || (start1 <= start2 && end1 >= end2))
+                // Проверка на пересечение: если один сеанс начинается до того, как другой закончится,
+                // и заканчивается после начала другого сеанса
+                if (start1 <= end2 && end1 >= start2)
                 {
-                    return true;
+                    return true; // Пересечение по времени найдено
                 }
             }
 
-            return false;
+            return false; // Нет пересечений
         }
-
     }
 }
